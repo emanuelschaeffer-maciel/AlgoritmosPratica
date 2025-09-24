0ab3e252104155b9d3e5ef50da5c97205d1f228e
Emanuel Maciel <emanuelschaeffer.maciel@estudante.ufjf.br>
2025-09-23 22:18:30 -0300

    Add files via upload

---
+++ Aula02/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    cout << "--------------------------" << endl;
+
+    cout << endl;
+
+    cout << "XXXXXX" << endl;
+    cout << "X" << endl;
+    cout << "XXXX" << endl;
+    cout << "X" << endl;
+    cout << "XXXXXX" << endl;
+    
+    cout << "\n\n\n" ;
+
+
+    
+    cout << "O compilador pode nos ajudar a identificar erros";
+   
+
+    cout << "\n\n\n";
+
+    cout << "Erros devem ser" ;
+    cout << "corrigidos na ordem em que sao identificados,";
+    cout << " pois ao corrigir ";
+    cout << "um erro \nOutros podem ser identificados" << endl;
+
+    cout << "\n" ;
+
+    cout << "--------------------------" << endl;
+
+    cout << "\n\n" ;
+
+
+
+
+ 
+
+    return 0;
+}
