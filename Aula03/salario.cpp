ca9827e204c5317d332cf9e8bfab2b2b9fce6fee
Emanuel Maciel <emanuelschaeffer.maciel@estudante.ufjf.br>
2025-09-30 22:56:17 -0300

    Create salario.cpp

---
+++ Aula03/salario.cpp
@@ -0,0 +1,57 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    cout << "1o valor: ";
+    int v1;
+    cin >> v1;
+    
+    cout << "2o valor: ";
+    int v2;
+    cin >> v2;
+    
+    v1 = v1 + v1%5;
+    v2 = v2 + v1%5;
+    
+    
+    
+    int v1_100 = v1/100;
+    v1 = v1%100
+    
+    int v1_50 = v1/50;
+    v1 = v1%50
+    
+    int v1_20 = v1/20;
+    v1 = v1%20;
+    
+    int v1_10 = v1/10;
+    v1 = v1%10;
+    
+    int v1_5 = v1/5;
+    v1 = v1%5;
+    
+    
+    int v2_100 = v2/100;
+    v2 = v2%100
+    
+    int v2_50 = v2/50;
+    v2 = v2%50
+    
+    int v2_20 = v2/20;
+    v2 = v2%20;
+    
+    int v2_10 = v2/10;
+    v2 = v2%10;
+    
+    int v1_5 = v2/5;
+    v2 = v2%5;
+    
+    
+    
+    
+
+    return 0;
+}
